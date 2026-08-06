c8e219f240b5c8fbad9ebc51e16738ddbe5af2ed
gPradeep0405 <gpradeep0405@gmail.com>
2026-08-06 06:53:09 +0530

    Time: 1 ms (97.68%), Space: 42.2 MB (95.94%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int mySqrt(int x) {
+        int low=1,high=x;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            // long val=mid*mid;
+            if((long)mid*mid==(long)x){
+                return mid;
+            }else if((long)mid*mid<(long)x){
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+        return high;
+    }
+}
